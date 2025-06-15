860faa2be3e8efb8b241e8e5df358fae5b0b9253
SayyedArslaan <166531908+Sayyedarslaan4712@users.noreply.github.com>
2025-06-15 21:55:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+const gallery = document.getElementById("gallery");
+
+for (let i = 10002; i <= 10051; i++) {
+  const img = document.createElement("img");
+  img.src = `images/${i}.jpg`;
+  img.alt = `Photo ${i}`;
+  img.addEventListener('click', () => openLightbox(img.src));
+  gallery.appendChild(img);
+}
